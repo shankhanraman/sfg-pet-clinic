427d72c22b2d7d067277219dca53a5b1bd8f4d6b
shankhanraman <shankhan.raman@gmail.com>
2025-06-27 16:19:24 +0530

    adding POJO. Closes #2.

---
+++ src/main/java/guru/springframework/sfg_pet_clinic/model/Pet.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfg_pet_clinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+}
